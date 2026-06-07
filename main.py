d4e52c6006bf62a836b54ec4097fda588f69985e
Abilash <abilashjayan123@gmail.com>
2026-06-07 16:53:38 +0530

    epic givin me the silent treatment

--- main.py
+++ main.py
@@ -200,16 +200,15 @@ def EpicPrice():
             args=["--no-sandbox","--disable-setuid-sandbox","--disable-blink-features=AutomationControlled"])
             context = browser.new_context(
                 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
-                ignore_https_errors=True
+                ignore_https_errors=True,
+                locale = "en-IN",
+                geolocation={"longitude":72.8777,"latitude":19.0760},
+                permissions=["geolocation"]
             )
             stealth = Stealth()
             stealth.apply_stealth_sync(context)
             page = context.new_page()
-            if "en-US" in epic_url :
-                indian_epic_url = epic_url.replace("en-US","en-IN")
-            else :
-                indian_epic_url = epic_url.replace("store.epicgames.com/","store.epicgames.com/en-IN/")
-            page.goto(indian_epic_url)
+            page.goto(epic_url)
             page.locator('strong:has-text("₹"), strong:has-text("Free")').wait_for(state="visible", timeout=10000)
             strong = page.locator("strong").all()
             epic_price = ""
